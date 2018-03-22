b4ce746c318cdb93f96a80a56f31ba378ad6929e
ahn <93immm@naver.com>
2018-03-22 14:09:13 +0900

    2018.3.22 짤방 수집기 개발

---
+++ dc.py
@@ -0,0 +1,58 @@
+#-*- coding:utf-8 -*-
+import requests
+import urllib
+from bs4 import BeautifulSoup
+
+fix_link = "http://gall.dcinside.com"
+upload_dir = '/Users/ahn/python/img/'
+
+url = 'http://gall.dcinside.com/board/lists/?id=baseball_new6&page=1'
+headers = {
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+'Accept-Encoding': 'gzip, deflate',
+'Accept-Language': 'ko-KR,ko;q=0.9,ja-JP;q=0.8,ja;q=0.7,en-US;q=0.6,en;q=0.5',
+'Cache-Control': 'max-age=0',
+'Connection': 'keep-alive',
+'Host': 'gall.dcinside.com',
+'Upgrade-Insecure-Requests': '1',
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+}
+
+r = requests.get(url, headers=headers)
+
+html = r.text
+
+soup = BeautifulSoup(html, "lxml")
+link = soup.find_all("td", { "class" : "t_subject" })
+for m in link:
+    if(m.find("a", {"class":"icon_pic_n"})):
+        post_number = m.parent.find("td", {"class" : "t_notice"}).string
+        post_title = m.a.string
+        print "글번호 = ", post_number
+        print "글제목 = ", post_title
+
+        inner_link = fix_link + m.a.get('href') + "\n"
+        print inner_link
+	#html = urllib.urlopen(inner_link)
+        r = requests.get(inner_link, headers=headers)
+        inner_html = r.text
+	soup = BeautifulSoup(inner_html, "lxml")
+	link2 = soup.find_all("li", { "class" : "icon_pic" })
+        #print link2
+        for n in link2:
+      	    pure_file_name = n.a.string
+            file_link = n.a.get('href')
+            print "파일명 = " + pure_file_name.encode('utf-8')
+            a = "http://image.dcinside.com/download.php"
+	    b = "http://dcimg2.dcinside.co.kr/viewimage.php"
+	    file_link = file_link.replace(a, b);
+            print "다운로드링크 = " + file_link
+            print "------------------------------------------------------------->"
+            r = requests.get(file_link, headers=headers)
+            fff = r.text
+            img_file = urllib.urlopen(file_link)
+            print fff
+            f = open(upload_dir + pure_file_name, 'wb')
+            #f.write(fff.read())
+            f.write(img_file.read())
+	    f.close()
